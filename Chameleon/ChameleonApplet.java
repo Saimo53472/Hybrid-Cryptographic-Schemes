0e2e1fa39c2d35099b5c6296cb5be5ad1e590aab
Saimo53472 <s.i.culachi@student.rug.nl>
2026-05-11 14:07:56 +0200

    restructured chameleon

--- Chameleon/ChameleonApplet.java
+++ Chameleon/ChameleonApplet.java
@@ -9,45 +9,33 @@ public class ChameleonApplet extends Applet {
 
     // Commands that can be sent from the terminal to control the applet behavior
     private static final byte INS_INIT = (byte) 0x10; // Initializes/resets session state
-    private static final byte INS_GENERATE_DELTA = (byte) 0x20; // Triggers generation of the Delta Certificate
-    private static final byte INS_GET_BASE_CERT = (byte) 0x30; // Returns the stored Base Certificate
-    private static final byte INS_GET_DELTA_CERT = (byte) 0x40; // Returns the computed Delta Certificate
-    private static final byte INS_SIGN = (byte) 0x50; // Creates the classical signature
-    private static final byte INS_SIGN_DELTA = (byte) 0x60; // Creates the post-quantum signature
-    private static final byte INS_GET_SIGN = (byte) 0x70; // Returns the computed signature
+    private static final byte INS_GET_CERT = (byte) 0x20; // Returns the stored Certificate
+    private static final byte INS_SIGN_BASE = (byte) 0x30; // Creates the classical signature
+    private static final byte INS_SIGN_DELTA = (byte) 0x40; // Creates the post-quantum signature
+    private static final byte INS_GET_SIG_BASE = (byte) 0x50; // Returns the computed classical signature
+    private static final byte INS_GET_SIG_DELTA = (byte) 0x60; // Returns the computed delta signature 
+
+    private Object data; // Data to be signed
 
     // TODO
     // Classical values 
     private ECPrivateKey classicalPrivateKey;
-    private ECPublicKey classicalPublicKey;
-    private Signature classicalSignature;
-
-    private byte[] signatureBuffer;
-    private short signatureLen;
 
     // Post-Quantum values
-    // private Object pqPublicKey;  
-    // private Object pqPrivateKey;  
-    // private Object pqEngine;      
+    private Object pqPrivateKey;
 
     // Certificate storage
-    private byte[] baseCertificate; // stored on card
-    private short baseCertLen;
-
-    private byte[] deltaCertificate; // derived from base certificate + DCD
-    private short deltaCertLen;
-
-    private byte[] dcd; // Delta Certificate Descriptor - defines how delta certificate is derived
-    private short dcdLen;
+    private byte[] certificate; // stored on card
+    private short certLen;
 
     // Temporary APDU buffer - communictaion
     private byte[] buffer;
 
+    private byte[] signatureBuffer;
+    private short signatureLen;
+
     protected ChameleonApplet() {
         buffer = new byte[255];
-
-        // initClassicalCrypto();
-        // initPostQuantumCrypto();
 
         register(); // makes the applet selectable 
     }
@@ -70,28 +58,24 @@ public class ChameleonApplet extends Applet {
                 initSession(apdu);
                 return;
 
-            case INS_GENERATE_DELTA:
-                generateDeltaCertificate(apdu);
-                return;
-
-            case INS_GET_BASE_CERT:
+            case INS_GET_CERT:
                 sendBaseCertificate(apdu);
                 return;
-
-            case INS_GET_DELTA_CERT:
-                sendDeltaCertificate(apdu);
-                return;
             
-            case INS_SIGN:
-                createSignature(apdu);
+            case INS_SIGN_BASE:
+                createSignatureBase(apdu);
                 return;
 
             case INS_SIGN_DELTA:
-                createDeltaSignature(apdu);
+                createSignatureDelta(apdu);
                 return;
 
-            case INS_GET_SIGN:
-                sendSignature(apdu);
+            case INS_GET_SIG_BASE:
+                sendSignatureBase(apdu);
+                return;
+
+            case INS_GET_SIG_DELTA:
+                sendSignatureDelta(apdu);
                 return;
 
             default:
@@ -99,75 +83,74 @@ public class ChameleonApplet extends Applet {
         }
     }
 
+    // TODO oof card
+    // private void initClassicalCrypto() {
+    //     classicalPrivateKey =
+    //     (ECPrivateKey) KeyBuilder.buildKey(
+    //         KeyBuilder.TYPE_EC_FP_PRIVATE,
+    //         KeyBuilder.LENGTH_EC_FP_256,
+    //         false
+    //     );
+
+    //     classicalPublicKey =
+    //     (ECPublicKey) KeyBuilder.buildKey(
+    //         KeyBuilder.TYPE_EC_FP_PUBLIC,
+    //         KeyBuilder.LENGTH_EC_FP_256,
+    //         false
+    //     );
+
+    //     classicalSignature =
+    //     Signature.getInstance(
+    //         Signature.ALG_ECDSA_SHA_256,
+    //         false
+    //     );
+
+    //     signatureBuffer = new byte[80];
+    // }
+
     // TODO
-    private void initClassicalCrypto() {
-        classicalPrivateKey =
-        (ECPrivateKey) KeyBuilder.buildKey(
-            KeyBuilder.TYPE_EC_FP_PRIVATE,
-            KeyBuilder.LENGTH_EC_FP_256,
-            false
-        );
-
-        classicalPublicKey =
-        (ECPublicKey) KeyBuilder.buildKey(
-            KeyBuilder.TYPE_EC_FP_PUBLIC,
-            KeyBuilder.LENGTH_EC_FP_256,
-            false
-        );
-
-        classicalSignature =
-        Signature.getInstance(
-            Signature.ALG_ECDSA_SHA_256,
-            false
-        );
-
-        signatureBuffer = new byte[80];
-    }
-
-    // TODO
-    private void initPostQuantumCrypto() {
-        pqPublicKey = null;
-        pqPrivateKey = null;
-        pqEngine = null;
-    }
+    // private void initPostQuantumCrypto() {
+    //     pqPublicKey = null;
+    //     pqPrivateKey = null;
+    //     pqEngine = null;
+    // }
 
     private void initSession(APDU apdu) {
-        baseCertLen = 0;
-        deltaCertLen = 0;
-        dcdLen = 0;
+        certLen = 0; // get
     }
 
-    // TODO
-    private void generateDeltaCertificate(APDU apdu) {
-        if (deltaCertificate == null) {
-            deltaCertificate = new byte[256];
-        }
-
-        short len = (short) 96; // placeholder
-
-        Util.arrayFillNonAtomic(deltaCertificate, (short) 0, len, (byte) 0x02); // sth like this??
-
-        deltaCertLen = len;
+    private void createSignatureBase(APDU apdu) {
+        classicalSignature.init(classicalPrivateKey, Signature.MODE_SIGN);
+        signatureLen = classicalSignature.sign(data, 0, (short) data.length, signatureBuffer, 0);
+        
+        apdu.setOutgoing();
+        apdu.setOutgoingLength(signatureLen);
+        apdu.sendBytes(signatureBuffer, 0, signatureLen);
     }
 
-    private void createSignature(APDU apdu) {
-        // TODO
+    private void createSignatureDelta(APDU apdu) {
+        pqSignature.init(pqPrivateKey, Signature.MODE_SIGN);
+        signatureLen = pqSignature.sign(data, 0, (short) data.length, signatureBuffer, 0);
+
+        apdu.setOutgoing();
+        apdu.setOutgoingLength(signatureLen);
+        apdu.sendBytes(signatureBuffer, 0, signatureLen);
     }
 
     // TODO: if the certificate / signature exceeds the buffer size
     private void sendBaseCertificate(APDU apdu) {
         apdu.setOutgoing();
-        apdu.setOutgoingLength(baseCertLen);
-        apdu.sendBytesLong(baseCertificate, (short) 0, baseCertLen);
+        apdu.setOutgoingLength(certLen);
+        apdu.sendBytesLong(certificate, (short) 0, certLen);
     }
 
-    private void sendDeltaCertificate(APDU apdu) {
+    private void sendSignatureBase(APDU apdu) {
         apdu.setOutgoing();
-        apdu.setOutgoingLength(deltaCertLen);
-        apdu.sendBytesLong(deltaCertificate, (short) 0, deltaCertLen);
+        apdu.setOutgoingLength(signatureLen);
+        apdu.sendBytesLong(signatureBuffer, (short) 0, signatureLen);
     }
 
-    private void sendSignature(APDU apdu) {
+    private void sendSignatureDelta(APDU apdu) {
         apdu.setOutgoing();
         apdu.setOutgoingLength(signatureLen);
         apdu.sendBytesLong(signatureBuffer, (short) 0, signatureLen);
--- README.md
+++ README.md
@@ -2,12 +2,22 @@
 
 ## Commands
 To use jdk17:
+MAC
 export JAVA_HOME=$(/usr/libexec/java_home -v 17)
 export PATH=$JAVA_HOME/bin:$PATH
 
+Pwsh
+$env:JAVA_HOME = "C:\Java\jdk-17\jdk-17.0.12"
+$env:PATH = "$env:JAVA_HOME\bin;$env:PATH"
+
 To use the Java card development kit:
+MAC
 export JC_HOME=~/Downloads/java_card_devkit_tools-bin-v25.1-b_611-26-OCT-2025
 export PATH=$JC_HOME/bin:$PATH
+
+Pwsh
+$env:JC_HOME = "$HOME/Downloads/java_card_devkit_tools-bin-v25.1-b_611-26-OCT-2025"
+$env:PATH = "$env:JC_HOME/bin;$env:PATH"
 
 To convert to class:
 javac -source 8 -target 8 -classpath $JC_HOME/lib/api_classic-3.0.5.jar Chameleon/ChameleonApplet.java 
