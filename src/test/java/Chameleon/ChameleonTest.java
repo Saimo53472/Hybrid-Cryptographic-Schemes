1bd08d2450347afc359508f395c04023e28397b1
Saimo53472 <s.i.culachi@student.rug.nl>
2026-07-16 16:45:09 +0200

    verified cert signatures

--- src/test/java/Chameleon/ChameleonTest.java
+++ src/test/java/Chameleon/ChameleonTest.java
@@ -17,29 +17,36 @@ import javax.smartcardio.*;
 
 import org.bouncycastle.pqc.crypto.hawk.HawkParameters;
 import org.bouncycastle.pqc.crypto.hawk.HawkPrivateKeyParameters;
+import org.bouncycastle.pqc.crypto.hawk.HawkPublicKeyParameters;
+import org.bouncycastle.pqc.crypto.hawk.HawkSigner;
 
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.interfaces.ECPrivateKey;
+
+import org.bouncycastle.asn1.ASN1OctetString;
+import org.bouncycastle.asn1.ASN1Primitive;
 
 public class ChameleonTest {
 
     private static final byte CLA = (byte) 0x00;
 
     // Metrics
-    private static int apduCount = 0;          // N_APDU
-    private static int bytesSent = 0;          // B_comm (host -> card)
-    private static int bytesReceived = 0;      // B_comm (card -> host)
+    private static int apduCount = 0; // N_APDU
+    private static int bytesSent = 0; // B_comm (host -> card)
+    private static int bytesReceived = 0; // B_comm (card -> host)
 
     // Timing
     private static long timeBaseSign = 0;
-    private static long timeDeltaSign = 0;     // will be used later
+    private static long timeDeltaSign = 0;
 
-    public static void main(String[] args) {
+    private static final String DCD_OID = "1.3.6.1.4.1.55555.1.100";
+
+    public static void main(String[] args) throws Exception {
 
         CardSimulator simulator = new CardSimulator();
 
         // Applet AID
-        byte[] aidBytes = {(byte) 0xA0, 0x00, 0x00, 0x00, 0x62, 0x01};
+        byte[] aidBytes = { (byte) 0xA0, 0x00, 0x00, 0x00, 0x62, 0x01 };
         AID aid = new AID(aidBytes, (short) 0, (byte) aidBytes.length);
 
         // Install + select
@@ -50,17 +57,17 @@ public class ChameleonTest {
 
         // 1. INIT
         ResponseAPDU response = send(simulator, new CommandAPDU(CLA, 0x10, 0x00, 0x00));
-        
+
         if (response.getSW() != 0x9000) {
             throw new RuntimeException("Assertion failed: expected 0x9000");
         }
 
         // 2. Load EC private key and certificate
         try {
-            byte[] key = loadECPrivateKey(Paths.get("src", "test", "resources", "keys", "key_pkcs8.pem")); // maybe chnage this
-            byte[] qkey = Files.readAllBytes(Paths.get("src","test","resources", "keys", "hawk512_private.key"));
+            byte[] key = loadECPrivateKey(Paths.get("src", "test", "resources", "keys", "key_pkcs8.pem"));
+            byte[] qkey = Files.readAllBytes(Paths.get("src", "test", "resources", "keys", "hawk512_private.key"));
             HawkPrivateKeyParameters sk = new HawkPrivateKeyParameters(HawkParameters.Hawk_512, qkey, 0, qkey.length);
-            byte[] cert = loadCertificate(Paths.get("src","test","resources", "certs", "chameleon_cert.pem")); 
+            byte[] cert = loadCertificate(Paths.get("src", "test", "resources", "certs", "chameleon_signed.crt"));
 
             send(simulator, new CommandAPDU(CLA, 0x70, 0x00, 0x00, key));
 
@@ -76,7 +83,7 @@ public class ChameleonTest {
 
             offset = 0;
             chunkSize = 200;
-            int S_cert = cert.length;  // certificate size in bytes
+            int S_cert = cert.length; // certificate size in bytes
             System.out.println("S_cert = " + S_cert);
 
             while (offset < cert.length) {
@@ -97,25 +104,44 @@ public class ChameleonTest {
 
         // 4. Get certificate
         int offset = 0;
+        ByteArrayOutputStream certBuffer = new ByteArrayOutputStream();
 
         while (true) {
             int p1 = (offset >> 8) & 0xFF;
             int p2 = offset & 0xFF;
 
             ResponseAPDU resp = send(simulator, new CommandAPDU(CLA, 0x20, p1, p2));
-
             byte[] data = resp.getData();
-
-            if (data.length == 0) break;
-
-            // store / append data
+            if (data.length == 0)
+                break;
+            certBuffer.write(data);
             offset += data.length;
-
-            if (data.length < 200) break; // last chunk
+            if (data.length < 200)
+                break; // last chunk
         }
+        byte[] receivedCert = certBuffer.toByteArray();
+
+        // 4*. Verify certificate
+        CertificateFactory cf = CertificateFactory.getInstance("X.509");
+        X509Certificate cert = (X509Certificate) cf.generateCertificate(new ByteArrayInputStream(receivedCert));
+
+        // 4.1 Verify ECDSA
+        cert.verify(cert.getPublicKey());
+        System.out.println("ECDSA: OK");
+
+        // 4.2 Extract DCD
+        byte[] dcd = unwrapExtension(cert.getExtensionValue(DCD_OID));
+
+        // 4.3 Parse DCD
+        DCDData data = DCDData.parseDCD(dcd);
+
+        // 4.4 Verify HAWK
+        byte[] deltaTbs = Files.readAllBytes(Paths.get("src/test/resources/certs/delta_tbs.der"));
+
+        verifyHAWK(data.getHawkPublicKey(), data.getHawkSignature(), deltaTbs);
 
         // 5. Internal authenticate (build dataToSign)
-        byte[] challenge = {0x01, 0x02, 0x03, 0x04};
+        byte[] challenge = { 0x01, 0x02, 0x03, 0x04 };
         send(simulator, new CommandAPDU(CLA, 0x88, 0x00, 0x00, challenge));
 
         // 6. Create classical signature
@@ -139,9 +165,9 @@ public class ChameleonTest {
         offset = 0;
 
         while (offset < signature.length) {
-            ResponseAPDU rsp = send(simulator, new CommandAPDU( CLA, 0x60, (offset >> 8) & 0xFF, offset & 0xFF));
+            ResponseAPDU rsp = send(simulator, new CommandAPDU(CLA, 0x60, (offset >> 8) & 0xFF, offset & 0xFF));
             byte[] chunk = rsp.getData();
-            System.arraycopy( chunk, 0, signature, offset, chunk.length);
+            System.arraycopy(chunk, 0, signature, offset, chunk.length);
             offset += chunk.length;
         }
 
@@ -189,7 +215,7 @@ public class ChameleonTest {
         }
         return sb.toString();
     }
-    
+
     private static byte[] loadECPrivateKey(Path path) throws Exception {
         byte[] keyBytes = Files.readAllBytes(path);
 
@@ -197,9 +223,9 @@ public class ChameleonTest {
 
         if (pem.contains("BEGIN")) {
             pem = pem
-                .replaceAll("-----BEGIN (.*)-----", "")
-                .replaceAll("-----END (.*)-----", "")
-                .replaceAll("\\s", "");
+                    .replaceAll("-----BEGIN (.*)-----", "")
+                    .replaceAll("-----END (.*)-----", "")
+                    .replaceAll("\\s", "");
 
             keyBytes = Base64.getDecoder().decode(pem);
         }
@@ -228,18 +254,33 @@ public class ChameleonTest {
         String pem = new String(Files.readAllBytes(pemPath));
 
         pem = pem
-            .replace("-----BEGIN CERTIFICATE-----", "")
-            .replace("-----END CERTIFICATE-----", "")
-            .replaceAll("\\s", "");
+                .replace("-----BEGIN CERTIFICATE-----", "")
+                .replace("-----END CERTIFICATE-----", "")
+                .replaceAll("\\s", "");
 
         return Base64.getDecoder().decode(pem);
     }
-
 
     public static PublicKey loadPublicKeyFromCert(String certPath) throws Exception {
         FileInputStream fis = new FileInputStream(certPath);
         CertificateFactory cf = CertificateFactory.getInstance("X.509");
         X509Certificate cert = (X509Certificate) cf.generateCertificate(fis);
         return cert.getPublicKey();
+    }
+
+    private static void verifyHAWK(byte[] hawkPublicKey, byte[] signature, byte[] message) throws Exception {
+        HawkPublicKeyParameters pk = new HawkPublicKeyParameters(HawkParameters.Hawk_512, hawkPublicKey, 0,
+                hawkPublicKey.length);
+        HawkSigner verifier = new HawkSigner();
+        verifier.init(false, pk);
+        boolean ok = verifier.verifySignature(message, signature);
+        System.out.println("HAWK signature: " + ok);
+    }
+
+    private static byte[] unwrapExtension(byte[] extension) throws Exception {
+        ASN1OctetString oct = ASN1OctetString.getInstance(
+                ASN1Primitive.fromByteArray(extension));
+
+        return oct.getOctets();
     }
 }
---
+++ src/test/java/Chameleon/DCDData.java
@@ -0,0 +1,88 @@
+package Chameleon;
+
+import java.util.Arrays;
+
+import org.bouncycastle.pqc.crypto.hawk.HawkParameters;
+import org.bouncycastle.pqc.crypto.hawk.HawkPublicKeyParameters;
+import org.bouncycastle.pqc.crypto.hawk.HawkSigner;
+
+import com.licel.jcardsim.bouncycastle.asn1.ASN1BitString;
+import com.licel.jcardsim.bouncycastle.asn1.ASN1OctetString;
+import com.licel.jcardsim.bouncycastle.asn1.ASN1Primitive;
+import com.licel.jcardsim.bouncycastle.asn1.ASN1Sequence;
+
+public class DCDData {
+
+    private byte[] hawkPublicKey;
+    private byte[] hawkSignature;
+
+    public static DCDData parseDCD(byte[] dcdBytes)
+            throws Exception {
+
+        ASN1Sequence dcd = ASN1Sequence.getInstance(
+                ASN1Primitive.fromByteArray(dcdBytes));
+
+        System.out.println("DCD elements: " + dcd.size());
+
+        ASN1Sequence spki = ASN1Sequence.getInstance(
+                dcd.getObjectAt(0));
+
+        ASN1BitString keyBits = ASN1BitString.getInstance(
+                spki.getObjectAt(1));
+
+        byte[] hawkPublicKey = keyBits.getBytes();
+        if (hawkPublicKey.length == 1025 && hawkPublicKey[0] == 0x00) {
+            hawkPublicKey = Arrays.copyOfRange(
+                    hawkPublicKey,
+                    1,
+                    hawkPublicKey.length);
+        }
+
+        ASN1OctetString sig = ASN1OctetString.getInstance(
+                dcd.getObjectAt(2));
+
+        DCDData result = new DCDData();
+
+        result.hawkPublicKey = hawkPublicKey;
+        result.hawkSignature = sig.getOctets();
+
+        System.out.println(
+                "HAWK public key length: "
+                        + hawkPublicKey.length);
+
+        System.out.println(
+                "HAWK signature length: "
+                        + result.hawkSignature.length);
+
+        return result;
+    }
+
+    public static void verifyHAWK(
+            byte[] hawkPublicKey,
+            byte[] signature,
+            byte[] message) throws Exception {
+        HawkPublicKeyParameters pk = new HawkPublicKeyParameters(
+                HawkParameters.Hawk_512,
+                hawkPublicKey,
+                0,
+                hawkPublicKey.length);
+
+        HawkSigner verifier = new HawkSigner();
+        verifier.init(false, pk);
+
+        boolean ok = verifier.verifySignature(
+                message,
+                signature);
+
+        System.out.println(
+                "HAWK signature: " + ok);
+    }
+
+    public byte[] getHawkPublicKey() {
+        return hawkPublicKey;
+    }
+
+    public byte[] getHawkSignature() {
+        return hawkSignature;
+    }
+}
